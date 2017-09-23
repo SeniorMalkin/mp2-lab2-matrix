81e375674abad5981b1653ef92d317b872922d45
SeniorMalkin <senior.malkin2014@yandex.ru>
2017-09-23 15:33:31 +0300

    Message

--- include/utmatrix.h
+++ include/utmatrix.h
@@ -77,6 +77,7 @@ TVector<ValType>::~TVector()
 template <class ValType> // доступ
 ValType& TVector<ValType>::operator[](int pos)
 {
+	return pVector[pos];
 } /*-------------------------------------------------------------------------*/
 
 template <class ValType> // сравнение
